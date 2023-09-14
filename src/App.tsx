f825d8b590e07b08f61ddace0a2738edb2a862db
Muzamil Bilwani <muzammil.bilwani@hotmail.com>
2023-09-14 22:40:56 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>
+  }
+])
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App
